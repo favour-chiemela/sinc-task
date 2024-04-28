52d0c13439a5159d4b4590b60b62afa1750de563
favour384 <desyfavour54@gmail.com>
2024-04-28 22:51:13 +0100

    Initial commit

---
+++ src/component/asin5-navbar/views/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from "../styles/Navbar.css";
+
+
+const Navbar = () => {
+  return (
+    <div>
+      <p>come</p>
+    </div>
+  )
+}
+
+export default Navbar
